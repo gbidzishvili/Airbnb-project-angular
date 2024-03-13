2e686d1bba30e78ee877ed4decc3ff77c2f9f518
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-03-13 14:55:43 +0400

    changedetection changed to onpush

--- ngAngularProject/src/app/app.module.ts
+++ ngAngularProject/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './navigation/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+
   ],
   imports: [
     BrowserModule,
